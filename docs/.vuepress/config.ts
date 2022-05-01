f47fb04e26a7d93d2fbbda7719c15b9147bdb36a
Fish <icyyfish@gmail.com>
2022-05-01 09:51:11 +0800

    update site config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,39 +4,52 @@ import type { DefaultThemeOptions } from "vuepress";
 export default defineUserConfig<DefaultThemeOptions>({
   base: "/grit/",
   lang: "en-US",
-  title: "Grit",
   description: "Workout",
   // theme and its config
   theme: "@vuepress/theme-default",
+  head: [['link', { rel: 'icon', href: '/images/fish.jpg' }]],
   themeConfig: {
-    // logo: "https://vuejs.org/images/logo.png",
+    logo: "/images/grit.png",
     navbar: [
       {
-        text: "GitHub",
-        link: "https://github.com/icyfish/grit"
+        text: "💪 Fitness",
+        link: "/fitness/",
+      },
+      {
+        text: "📚 Books",
+        link: "/books/",
       }
     ],
     sidebar: [
       {
-        text: "Fitness",
+        text: "📌 Home",
+        link: "/home/"
+      },
+      {
+        text: "💪 Fitness",
+        link: "/fitness/",
         children: [
           {
             text: "Grit Cardio",
             children: [
-              { text: "✅ Grit Cardio 22", link: "/grit-22/cardio.md" },
-              { text: "🌟 Grit Cardio 23", link: "/grit-23/cardio.md" },
-              { text: "🌟 Grit Cardio 39", link: "/grit-39/cardio.md" },
+              { text: "✅ Grit Cardio 22", link: "/fitness/grit-22/cardio.md" },
+              { text: "🌟 Grit Cardio 23", link: "/fitness/grit-23/cardio.md" },
+              { text: "🌟 Grit Cardio 39", link: "/fitness/grit-39/cardio.md" },
               {
-                text: "🐟's Workout Tracker",
-                link: "/my/cardio.md"
+                text: "🐟's Cardio Tracker",
+                link: "/fitness/my/cardio.md"
               }
             ]
           },
           {
             text: "📝 Terms",
-            link: "/terms/index.md"
+            link: "/fitness/terms/index.md"
           }
         ]
+      },
+      {
+        text: "📚 Books",
+        link: "/books/",
       }
     ]
   },
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,9 @@
-# Grit
-
-::: tip Grit
-is picking yourself up and moving forward even when you think you can’t take one more step.
-:::
+---
+home: true
+heroImage: /images/strong.jpeg
+tagline: GRIT is picking yourself up and moving forward even when you think you can’t take one more step
+actions:
+  - text: Home
+    link: /home/
+footer: 我不愿一生晒太阳吹风, 咸鱼也要有梦
+---
