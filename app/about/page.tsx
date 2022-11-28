ccc70f6103eadd4b5c060029e5cc5b51fa7827f5
Fish <bingyuzhang@trip.com>
2022-11-28 11:43:47 +0800

    init project

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function About() {
+    return (
+        <div>
+            About
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import './globals.css'
+import headerNavLinks from "@/data/headerNavLinks"
+import siteMetadata from "@/data/siteMetadata"
+import Link from '@/components/Link'
+import LayoutContainer from '@/components/LayoutContainer'
+import MobileNav from '@/components/MobileNav'
+import Footer from '@/components/Footer'
+// import ThemeSwitch from '@/components/ThemeSwitch'
 
 export default function RootLayout({
   children,
@@ -7,12 +14,59 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <LayoutContainer>
+          <div className="flex h-screen flex-col justify-between">
+            <Header />
+            <main className="mb-auto">{children}</main>
+            <Footer />
+          </div>
+        </LayoutContainer>
+      </body>
     </html>
   )
+}
+
+function Header() {
+  return <header className="flex items-center justify-between py-10">
+    <SiteInfo />
+    <HeaderNav />
+    {/* <ThemeSwitch /> */}
+    <MobileNav />
+  </header>
+}
+
+function SiteInfo() {
+  return <div>
+    <Link href="/" aria-label={siteMetadata.headerTitle}>
+      <div className="flex items-center justify-between">
+        <div className="mr-3">
+          {/* <Logo /> */}
+        </div>
+        {typeof siteMetadata.headerTitle === 'string' ? (
+          <div className="hidden h-6 text-2xl font-semibold sm:block">
+            {siteMetadata.headerTitle}
+          </div>
+        ) : (
+          siteMetadata.headerTitle
+        )}
+      </div>
+    </Link>
+  </div>
+}
+
+function HeaderNav() {
+  return <div className="flex items-center text-base leading-5">
+    <div className="hidden sm:block">
+      {headerNavLinks.map((link) => (
+        <Link
+          key={link.title}
+          href={link.href}
+          className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+        >
+          {link.title}
+        </Link>
+      ))}
+    </div></div>
 }
